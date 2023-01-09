4cf772353a82db006c3c7e53cd2a82a20fc4e9a1
Devilriya <logesh.12a3@gmail.com>
2023-01-09 13:30:35 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -29,7 +29,7 @@ class Var(object):
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', '192.46.215.23:8888') if not ON_HEROKU or getenv('FQDN', '192.46.215.23:8888') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', '192.46.215.23:8888')) if not ON_HEROKU or getenv('FQDN', '192.46.215.23:8888') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)

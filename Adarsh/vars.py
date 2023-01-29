07e92bbb8107e68c3e5db7691c7d0eb4d379a1dd
Devilriya <logesh.12a3@gmail.com>
2023-01-29 06:59:35 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -36,6 +36,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Logesh:Logesh2004@cluster0.rs4dfrs.mongodb.net/?retryWrites=true&w=majority'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://abcd:abcd@cluster0.9gfi3ri.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Logeshbots'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
